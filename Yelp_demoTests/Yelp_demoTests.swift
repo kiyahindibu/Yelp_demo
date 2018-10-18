5713bff896c5a339962b55231b82cf2f24a0f596
Kiya👱🏽 <kiyasegni@kiyas-macbook-air.local>
2018-10-18 11:49:10 -0400

    Initial Commit

---
+++ Yelp_demoTests/Yelp_demoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Yelp_demoTests.swift
+//  Yelp_demoTests
+//
+//  Created by Kiya Segni on 10/18/18.
+//  Copyright © 2018 Kiya Segni. All rights reserved.
+//
+
+import XCTest
+@testable import Yelp_demo
+
+class Yelp_demoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
